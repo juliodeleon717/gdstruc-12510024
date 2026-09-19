4de2f475c817b9bd8d75b39b4fb0281b3456a675
BreadFreezer <juliodeleon0717@gmail.com>
2026-09-19 14:44:31 +0800

    added sample project

---
+++ test/untitled/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args){
+        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is yo name");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
